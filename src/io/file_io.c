2c9e128542fb1d1855d7f551954c1bab80e259dd
Amir Shavitt <amirshav@localhost.localdomain>
2015-02-10 15:33:06 +0200

    implemented load_game in file_io.c

--- src/io/file_io.c
+++ src/io/file_io.c
@@ -1,3 +1,5 @@
+#include "file_io.h"
+
 int save_game(game* gamep, FILE* file) {
 	int error;
 	struct board = (gamep->board)*;
@@ -12,13 +14,86 @@ int save_game(game* gamep, FILE* file) {
 	board[gamep->mouse_x][gamep->mouse_y] = MOUSE;
 	board[gamep->cheese_x][gamep->cheese_y] = CHEESE;
 	// print the board
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE; j++) {
-			error = fprintf(file, "%c", board[i][j];
+	for (int i = SIZE-1; i >= 0; i++) { // Y coordinate
+		for (int j = 0; j < SIZE; j++) { // X coordinate
+			error = fprintf(file, "%c", board[j][i];
 			CHECK(error)
 		}
 		error = fprintf(file, "\n");  //FIXME what about the last line break?
 		CHECK(error)
 	}
+	return 0;
+}
+
+void find_obj(game* gamep, char obj) {
+	int x = 0, y = 0;
+	for (int i = 0; i < SIZE; i++) { // Y coordinate
+		for (int j = 0; j < SIZE; j++) { // X coordinate
+			if (gamep->board->board[j][i] == obj) {
+				x = j;
+				y = i;
+				gamep->board->board[j][i] = EMPTY //empty the cell
+			}
+			break;
+		}
+	}
+	// set the obj coordinates
+	if (obj == CAT) {
+		cat_x = x;
+		cat_y = y;
+	}
+	if (obj == MOUSE) {
+		mouse_x = x;
+		mouse_y = y;
+	}
+	if (obj == CHEESE) {
+		cheese_x = x;
+		cheese_y = y;
+	}
+}
+
+int load_game(game* gamep, FILE* file) {
+	int error;
+	const char delim[2] = "\n";
+	char* line;
+	char buffer[128]; // a buffer to read the file into
+	error = fgets(buffer, 128, file);
+	CHECK(error)
+	// get the first line
+	line = strtok(buffer, delim);
+	if (line == NULL) { //this SHOULDN'T happen, we assume validity
+		perror("Error: world file is not valid");
+		return 1;
+	}
+	error = sscanf(line, "%d", &(gamep->turns)) //load the number of turns left
+	CHECK(error)
+	
+	// get the second line
+	line = strtok(buffer, delim);
+	if (line == NULL) { //this SHOULDN'T happen, we assume validity
+		perror("Error: world file is not valid");
+		return 1;
+	}
+	gamep->player = strcmp("cat\n",line) ? MOUSE : CAT; //set the next player
+
+	// get the next lines
+	line = strtok(buffer, delim);
+	for (int i = SIZE-1; i >= 0; i--) { // Y coordinate
+		if (line == NULL) { //this SHOULDN'T happen, we assume validity
+			perror("Error: world file is not valid");
+			return 1;
+		}
+		for (int j = 0; j < SIZE; j++) { // X coordinate
+			gamep->board->board[j][i] = line[j];
+		}
+		//we move to the next line of the board
+		line = strtok(buffer, delim);
+	}
+		
+	//now we will look for the mouse, cat, and cheese in the board
+	find_obj(gamep, MOUSE);
+	find_obj(gamep, CAT);
+	find_obj(gamep, CHEESE);
+
 	return 0;
 }
