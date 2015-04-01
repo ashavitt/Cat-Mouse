4968385ee0a5ae284eb338fdd293ffc7c35ec78c
Nitzan Pomerantz <pomerantz@mail.tau.ac.il>
2015-04-01 13:30:37 +0300

    build main menu continued

--- src/gui/uitree.c
+++ src/gui/uitree.c
@@ -7,8 +7,14 @@ int build_main_menu(Widget* window, game_state* state) {
 	}
 
 	children = newList(NULL);
+
+	// Create the first graphic figure, taken from the spritesheet imgsrc - TODO add it somewhere
+	SDL_Rect dims = {30,30,100,20}; //x y w h
+	SDL_Rect pos = {30,30,100,20};
+	append(children, newGraphic(id, dims, pos, SDL_Surface* imgsrc));
 }
 
+ //another parameter should be (array) SDL_Surface** sprite sheets? Or they should be global
 int build_ui(Widget* window, game_state* state) {
 	switch (state->type) {
 		case MAIN_MENU:
