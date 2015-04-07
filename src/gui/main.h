f3b885f3a124a283ac89d6eb8c8965c72bc6519d
Amir Shavitt <ashavitt@gmail.com>
2015-04-07 15:08:08 +0300

    removed double defines

--- src/gui/main.h
+++ src/gui/main.h
@@ -11,27 +11,4 @@
 #define SCREEN_HEIGHT 800
 #define BPP 32 //bits per pixel
 
-
-typedef enum  {
-	MAIN_MENU, CHOOSE_PLAYER, CHOOSE_SKILL, IN_GAME, SELECT_GAME, GAME_EDIT, ERROR_DIALOG
-} state_type;
-
-typedef struct  {
-	state_type type;
-	int focused; 
-	// focused 0-99 is regular buttons
-	// if focused>99 then focused = 1xy in world editing
-	int number; //skill_level or world_id
-	Game* game; // for world editing
-} game_state;
-
-#ifndef __main_c__
-extern SDL_Surface* texts;
-extern SDL_Surface* buttons;
-extern SDL_Surface* cat;
-extern SDL_Surface* mouse;
-extern SDL_Surface* tiles;
-extern SDL_Surface* grid;
-#endif
-
 int run_gui();
