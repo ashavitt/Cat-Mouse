a0f81e84250a2b619cb90182020bc34e67ddb101
Amir Shavitt <ashavitt@gmail.com>
2015-04-07 13:46:47 +0300

    added errors & changed numbering to decimal

--- src/error/error.h
+++ src/error/error.h
@@ -2,7 +2,6 @@
 #define __ERROR__
 
 
-
 /*** GUI ***/
 /** Widget **/
 
@@ -10,12 +9,26 @@
 
 
 /* add_rect */
-#define ERROR_NO_RECT1 0x01
-#define ERROR_NO_RECT2 0x02
+#define ERROR_NO_RECT1 1
+#define ERROR_NO_RECT2 2
 
 /* draw_widget */
-#define ERROR_NO_WIDGET 0x10
-#define ERROR_NO_WINDOW 0x11
-#define ERROR_BLIT_FAIL 0x12
+#define ERROR_NO_WIDGET 10
+#define ERROR_NO_WINDOW 11
+#define ERROR_BLIT_FAIL 12
+
+/** UITree **/
+
+/* build_ui */
+#define ERROR_APPEND_FAILED 20
+
+/** main **/
+
+/* run_gui */
+#define ERROR_BUILD_UI_FAILED 30
+#define ERROR_WIDGET_FACTORY_FAILED 31
+
+/*** General ***/
+#define ERROR_MALLOC_FAILED 100
 
 #endif
