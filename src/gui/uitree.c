2bce42aa2e39fcb0c642f06d9a402038c7839d70
Nitzan Pomerantz <nitzpo@gmail.com>
2015-04-08 19:55:31 +0300

    Added build_choose_player
    Also added quit_action and back_action in relavent places.
    There is some major code duplication here, and there will be much more if we continue this way...
    Must add titles. Note: Titles can be depended on game_state fields that are not yet defined, like if we set for cat or mouse.

--- src/gui/uitree.c
+++ src/gui/uitree.c
@@ -110,7 +110,7 @@ int build_main_menu(Widget* window, game_state* state) {
 	}
 	text_dims.x = MAIN_MENU_T_X_START + WL_T_W;
 	text_dims.y = MAIN_MENU_T_Y_START + WL_T_H;
-	button = build_text_button(QUIT_B, button_pos, button_dims, text_dims, panel, NULL);
+	button = build_text_button(QUIT_B, button_pos, button_dims, text_dims, panel, quit_action);
 	if (append(panel->children, button) == NULL) {
 		return ERROR_APPEND_FAILED;
 	}
@@ -133,10 +133,92 @@ int build_main_menu(Widget* window, game_state* state) {
 	return 0;
 }
 
+int build_choose_player(Widget* window, game_state* state) {
+	int err;
+	Widget* panel;
+	Widget* button;
+	// TODO add title
+	//height is height of 3 buttons + 0.5 button per spacing between buttons
+	SDL_Rect panel_dims = {0, 0, WL_BUTTON_W, WL_BUTTON_H*4};
+	SDL_Rect button_dims = {0, 0, WL_BUTTON_W, WL_BUTTON_H};
+	SDL_Rect button_pos = {0, 0, WL_BUTTON_W, WL_BUTTON_H};
+	SDL_Rect button_offset = {0, WL_BUTTON_H * 1.5, 0, 0};
+	SDL_Rect text_dims;
+	
+	ListRef children, buttons;
+	if ((children = window->children) != NULL) {
+		destroyList(children, &freeWidget);
+	}
+
+	children = newList(NULL);
+
+	//creating a new panel for the buttons
+	panel = new_panel(UNFOCUSABLE, get_center(window->dims, panel_dims), window);
+	if (append(children, panel) == NULL) {
+		return ERROR_APPEND_FAILED;
+	}
+	
+	/*-----creating choose player buttons-----*/
+	//human
+	button_dims.x = 0;
+	button_dims.y = WL_BUTTON_H;
+	text_dims.x = MAIN_MENU_T_X_START + 2*WL_T_W;
+	text_dims.y = MAIN_MENU_T_Y_START + WL_T_H;
+	text_dims.w = WL_T_W;
+	text_dims.h = WL_T_H;
+	button = build_text_button(HUMAN_B, button_pos, button_dims, text_dims, panel, NULL);
+	if (append(panel->children, button) == NULL) {
+		return ERROR_APPEND_FAILED;
+	}
+
+	//machine
+	if ((err = add_rect(&button_pos, &button_offset)) != 0) {
+		printf("Error in add_rect: %d\n", err);
+		return ERROR_ADD_RECT_FAILED;
+	}
+	text_dims.x = 0;
+	text_dims.y = MAIN_MENU_T_Y_START + 2*WL_T_H;
+	button = build_text_button(MACHINE_B, button_pos, button_dims, text_dims, panel, NULL);
+	if (append(panel->children, button) == NULL) {
+		return ERROR_APPEND_FAILED;
+	}
+
+	//back
+	if ((err = add_rect(&button_pos, &button_offset)) != 0) {
+		printf("Error in add_rect: %d\n", err);
+		return ERROR_ADD_RECT_FAILED;
+	}
+	text_dims.x = MAIN_MENU_T_X_START + WL_T_W;
+	text_dims.y = MAIN_MENU_T_Y_START + 2*WL_T_H;
+	button = build_text_button(BACK_B, button_pos, button_dims, text_dims, panel, back_action);
+	if (append(panel->children, button) == NULL) {
+		return ERROR_APPEND_FAILED;
+	}
+
+	//change the background color of the focused button
+	buttons = panel->children;
+	button_dims.y *= 2; // ONLY APPLIES FOR MAIN MENUS
+	while (buttons != NULL) {
+		if (headData(buttons) != NULL) {
+			button = ((Widget*)headData(buttons));
+			if (button->id == state->focused) {
+				//set the correct color for the button
+				((Widget*)headData(button->children))->dims = button_dims;
+			}
+		}
+		buttons = tail(buttons);
+	}
+
+	window->children = children;
+	return 0;
+}
+
 int build_ui(Widget* window, game_state* state) {
 	switch (state->type) {
 		case MAIN_MENU:
 			return build_main_menu(window, state);
+		case CHOOSE_PLAYER:
+			return build_choose_player(window, state);
 		default:
 			return 0;
 	}
