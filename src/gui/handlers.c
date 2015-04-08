92e5926225b157f4284dc7bf77a9489e3684c639
Nitzan Pomerantz <nitzpo@gmail.com>
2015-04-08 19:25:50 +0300

    Added handle_keyboard

--- src/gui/handlers.c
+++ src/gui/handlers.c
@@ -21,9 +21,10 @@ int handle_mouse_click_rec(SDL_Event* e, Widget* widget, game_state* state, SDL_
 			if ((err = (widget->onclick)(widget, state)) != 0) {
 				printf("Error in onclick func of widget, code %d\n",err);
 				return err;
+			}
 		}
 	}
-
+//???
 	//TODO later we should cut the graphics to fit in the panel ???
 	if (widget->type == PANEL) {
 		abs_pos.w = (widget->pos).w;
@@ -49,4 +50,39 @@ int handle_mouse_click_rec(SDL_Event* e, Widget* widget, game_state* state, SDL_
 int handle_mouse_click(SDL_Event* event, Widget* window, game_state* state) {
 	SDL_Rect zeros = {0,0,0,0};
 	return handle_mouse_click_rec(event, window, state, zeros);
+}
+
+int handle_keyboard(SDL_Event* event, Widget* window, game_state* state) {
+	Widget* widget;
+	int err;
+	switch(event->key.keysym.sym) {
+		case SDLK_ESCAPE:
+			return 1;
+		case SDLK_RETURN:
+			widget = find_widget_by_id(window, state->focused);
+			if (widget == NULL) {
+				// focused widget must exist
+				printf("ERROR no widget with id:%d was found",state->focused);
+				return ERROR_FOCUSED_ID;
+			}
+			if ((err = (widget->onclick)(widget, state)) != 0) {
+				// "NULL pointer exception"
+				printf("Error in onclick func of widget, code %d\n",err);
+				return err;
+			}
+			break;
+		case SDLK_TAB:
+			// move to next focusable
+			break;
+		case SDLK_UP:
+		case SDLK_DOWN:
+		case SDLK_RIGHT:
+		case SDLK_LEFT:
+			if (state->type == IN_GAME || state->type == GAME_EDIT) {
+				//move cursor to direction event->key.keysym.sym
+			}
+			break;
+		//cases for in-game or edit-game later
+	}
+	return 0;
 }
