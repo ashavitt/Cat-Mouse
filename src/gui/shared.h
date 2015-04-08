fa105a1d1ab03ba2f5c1d8b6d24a800353f7e3f7
Nitzan Pomerantz <nitzpo@gmail.com>
2015-04-08 17:39:41 +0300

    Added previous_state to game_state

--- src/gui/shared.h
+++ src/gui/shared.h
@@ -6,13 +6,14 @@ typedef enum  {
 	MAIN_MENU, CHOOSE_PLAYER, CHOOSE_SKILL, IN_GAME, SELECT_GAME, GAME_EDIT, ERROR_DIALOG
 } state_type;
 
-typedef struct  {
+typedef struct game_state_struct {
 	state_type type;
 	int focused; 
 	// focused 0-99 is regular buttons
 	// if focused>99 then focused = 1xy in world editing
 	int number; //skill_level or world_id
 	Game* game; // for world editing
+	game_state_struct* previous_state;
 } game_state;
 
 #ifndef __main_c__
