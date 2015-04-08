b80900091b9d5a917be4e9fdf8447fc0f6270148
Amir Shavitt <ashavitt@gmail.com>
2015-04-08 14:03:29 +0300

    started build main menu

--- src/gui/uitree.c
+++ src/gui/uitree.c
@@ -24,12 +24,32 @@ Widget* build_text_button(int id, SDL_Rect main_pos, SDL_Rect bg_dims, SDL_Rect 
 
 int build_main_menu(Widget* window, game_state* state) {
 	int id = 2; //TODO change this
+	Widget* panel;
+	Widget* button;
+	//height is height of 5 buttons + 0.5 button per spacing between buttons
+	SDL_Rect panel_dims = {0, 0, WL_BUTTON_W, WL_BUTTON_H*7};
+	SDL_Rect button_dims = {0, 0, WL_BUTTON_W, WL_BUTTON_H};
+	SDL_Rect button_pos = {0, 0, WL_BUTTON_W, WL_BUTTON_H};
+	SDL_Rect text_dims;
+	
 	ListRef children;
 	if ((children = window->children) != NULL) {
 		destroyList(children, &freeWidget);
 	}
 
 	children = newList(NULL);
+
+	//creating a new panel for the buttons
+	panel = new_panel(UNFOCUSABLE, get_center(window->dims, panel_dims), window);
+	
+	//-----creating the buttons-----
+	//new game
+	button_dims.x = 0;
+	button_dims.y = WL_BUTTON_H;
+	text_dims.x = MAIN_MENU_T_X_START;
+	text_dims.y = MAIN_MENU_T_Y_START;
+	button = build_text_button(NEW_GAME_B, button_pos, button_dims, text_dims, panel, NULL);
+
 
 	SDL_Rect dims = {30,30,100,20}; //x y w h
 	SDL_Rect pos = {30,30,100,20};
