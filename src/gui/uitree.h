307e690089b7bdb5ceb7051d1b0318df6d51edcf
nitzpo <nitzpo@gmail.com>
2015-04-08 13:15:54 +0300

    Added text pos,dims and nums pos,dims

--- src/gui/uitree.h
+++ src/gui/uitree.h
@@ -8,10 +8,10 @@
 
 int build_ui(Widget*, game_state*);
 
-/** Sprites **/
+/*** Sprites ***/
 
-/* Buttons */
-//WL - Wide long, NL - Narrow long, WS - Wide short, NS - Narrow short
+/** Buttons **/
+//WL - Wide long, NL - Narrow long, WS - (very) Wide short, NS - Narrow short
 #define WL_BUTTON_W 282
 #define WL_BUTTON_H 57
 #define NL_BUTTON_W 141
@@ -19,7 +19,39 @@ int build_ui(Widget*, game_state*);
 #define WS_BUTTON_W WL_BUTTON_W
 #define WS_BUTTON_H 25
 
-/* Text */
+/** Text **/
+#define WL_T_W 264
+#define WL_T_H 51
+#define NL_T_W 132
+#define NL_T_H 51
+
+/* Titles */
+#define TITLES_T_X_START 528
+#define TITLES_T_Y_START 271
+
+/* Numbers */
+// Small Numbers (starts with braces () )
+#define S_NUM_T_X_START 0
+#define S_NUM_T_Y_START 102
+#define S_NUM_T_W 10
+#define S_NUM_T_H 16
+// Medium Numbers (starts with 0)
+#define M_NUM_T_X_START 0
+#define M_NUM_T_Y_START 577
+#define M_NUM_T_W 11
+#define M_NUM_T_H 15
+// Large Numbers (starts with 0)
+#define L_NUM_T_X_START 0
+#define L_NUM_T_Y_START 592
+#define L_NUM_T_W 14
+#define L_NUM_T_H 20
+
+/* Main Menu */
+// 5 buttons, 3 in a row
+#define MAIN_MENU_T_X_START 0
+#define MAIN_MENU_T_Y_START 118
+
+/* Messages */
 
 /** General **/
 #define BACK_B 1
