2e6220600ced3b9053ece1d4a2d5d17490bb5454
Nitzan Pomerantz <nitzpo@gmail.com>
2015-04-09 17:26:39 +0300

    removed ids from here

--- src/gui/uitree.h
+++ src/gui/uitree.h
@@ -54,52 +54,6 @@ int build_ui(Widget*, game_state*);
 
 /* Messages */
 
-/** General **/
-#define BACK_B 1
-#define QUIT_B 6
-#define DONE_B 12
-#define UNFOCUSABLE 100
 
-/** Main Menu **/
-#define NEW_GAME_B 2
-#define LOAD_GAME_B 3
-#define CREATE_GAME_B 4
-#define EDIT_GAME_B 5
-
-/** New Game **/
-#define HUMAN_B 7
-#define MACHINE_B 8
-
-/** Choose Skill Level **/
-#define LEVEL_CHOOSER 9
-#define LEVEL_UP_B 10
-#define LEVEL_DN_B 11
-
-/** Game Screen **/
-#define PAUSE_B 13
-#define RECONF_MOUSE_B 14
-#define RECONF_CAT_B 15
-#define RESTART_GAME_B 16
-#define GOTO_MAIN_MENU_B 17
-
-/** Error Message **/
-//back button
-
-/** World Building **/
-/* Load Game / Save World / Edit Game */
-#define WORLD_CHOOSER 18
-#define WORLD_UP_B LEVEL_UP_B
-#define WORLD_DN_B LEVEL_DN_B
-
-/* World Editing */
-#define SAVE_WORLD_B 19
-#define PLACE_MOUSE_B 20
-#define PLACE_CAT_B 21
-#define PLACE_CHEESE_B 22
-#define PLACE_WALL_B 23
-#define PLACE_SPACE_B 24
-
-/** ETC **/
-#define GRID_B 25
 
 #endif
