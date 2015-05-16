3d98a5f50586cb1d75bfd33a6bdf49c0b377af02
Nitzan Pomerantz <pomerantz@mail.tau.ac.il>
2015-05-16 12:29:29 +0300

    Added comments

--- src/gui/shared.c
+++ src/gui/shared.c
@@ -1,25 +1,31 @@
 #include "shared.h"
 
+/*** shared.c
+ * General functions for use in gui
+ ***/
 
+/** State Freeing
+ * Frees the given state's predecessors
+ * Two versions: one gets the actual state, one gets a pointer
+ **/
 void free_state(game_state state) { // doesn't free state!!
-	if (state.previous_state != NULL) {
-		free_state(*(state.previous_state));
+	if (state.previous_state != NULL) { // if the previous state is not NULL
+		free_state(*(state.previous_state)); // free it's predecessors
 	}
-	free(state.previous_state);
-	state.previous_state = NULL;
+	free(state.previous_state); // free it
+	state.previous_state = NULL; // set pointer to NULL
 	return;
 }
-
 void free_prev_states(game_state* state) {
-	if (state == NULL) {
+	if (state == NULL) { // state points to NULL so we do nothing
 		return;
 	}
 	game_state* p = state->previous_state, *q;
-	while (p != NULL) {
+	while (p != NULL) { // run over previous states and free them
 		q = p->previous_state;			
 		free(p);
 		p = q;
 	}
-	state->previous_state = NULL;
+	state->previous_state = NULL; // no there is no previous state
 	return;
 }
