afdd8a22442e80e050b03937ac2b0f86270d8078
Amir Shavitt <ashavitt@gmail.com>
2015-05-17 18:33:30 +0300

    changed doge

--- src/main/CatAndMouse.c
+++ src/main/CatAndMouse.c
@@ -1,27 +1,29 @@
 #include "CatAndMouse.h"
 
 /**
-░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░So Cat
-░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░
-░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░
-░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░
-░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░Much Mouse
-░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░
-░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░
-░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░
-░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░
-░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░
-▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░
-▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
-▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░
-░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░
-░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░
-░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░
-░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░
-░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
-░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░
+                Y.                      _   
+                YiL                   .```.  
+                Yii;                .; .;;`.    
+                YY;ii._           .;`.;;;; :   So cat
+                iiYYYYYYiiiii;;;;i` ;;::;;;;    
+            _.;YYYYYYiiiiiiYYYii  .;;.   ;;; 
+         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;    
+       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..   
+      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.    
+      Y$MM$:   :YYYYYY$!"``"4YYYYYiiiYYYYiiYY.    Much mouse
+   `. :MM$$b.,dYY$$Yii" :'   :YYYYllYiiYYYiYY    
+_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY
+.,._ $b`P`     "4$$$$$iiiiiiii$$$$YY$$$$$$YiY;
+   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL
+    "`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:  
+    ;$P$;;: ;;;;i$y$"!Y$$$b;$$$Y$YY$$YYYiiiYYiYY 
+    $Fi$$ .. ``:iii.`-":YYYYY$$YY$$$$$YYYiiYiYYY    
+    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:    
+     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY. 
+      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY    
+      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY    
+     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'    
 **/
-//#define CONSOLE_QUIT_CODE 10
 
 int main(int argc, char* argv[]) {
 	int eval, quit = 1;
